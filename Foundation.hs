39653d746d36e5ddf185e52d6dce7d1a9a94b68e
Matthew Elder <matt@mattelder.org>
2012-04-15 16:58:30 -0400

    remove extraneous import of static files

--- Foundation.hs
+++ Foundation.hs
@@ -32,9 +32,6 @@ import Yesod.Auth.Dummy
 import Yesod.Auth.Email
 import Data.Text
 
--- bind files to toplevel
-$(staticFiles "static")
-
 -- | The site argument for your application. This can be a good place to
 -- keep settings and values requiring initialization before your application
 -- starts running, such as database connections. Every handler will have
