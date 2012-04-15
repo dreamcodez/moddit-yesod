99bb00ff2dd519ec789a6cc520af039ea15bd9c3
Matthew Elder <matt@mattelder.org>
2012-04-15 16:24:20 -0400

    static file bindings were not being spliced in, add the TH to do that

--- Foundation.hs
+++ Foundation.hs
@@ -32,6 +32,9 @@ import Yesod.Auth.Dummy
 import Yesod.Auth.Email
 import Data.Text
 
+-- bind files to toplevel
+$(staticFiles "static")
+
 -- | The site argument for your application. This can be a good place to
 -- keep settings and values requiring initialization before your application
 -- starts running, such as database connections. Every handler will have
